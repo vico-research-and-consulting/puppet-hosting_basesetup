a865d353decf9b10de2655b3dbca1dc9154ca55e
Marc Schoechlin <ms@256bit.org>
2018-01-30 08:32:04 +0100

    Add missing notify

--- manifests/kernel/sysctl.pp
+++ manifests/kernel/sysctl.pp
@@ -6,11 +6,13 @@ define hosting_basesetup::kernel::sysctl (String $ensure = 'present', $value) {
       context => $context,
       onlyif  => "get ${title} != '${value}'",
       changes => "set ${title} '${value}'",
+      notify  => Exec['sysctl_file_load'],
     }
   } else {
     augeas { "sysctl_conf/${title}":
       context => $context,
       changes => "rm ${title}",
+      notify  => Exec['sysctl_file_load'],
     }
   }
 }
