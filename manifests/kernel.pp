1713509f21bc3a14aff34dd65b98538126b2209a
Marc Schoechlin <ms@256bit.org>
2018-02-07 09:45:51 +0100

    Settings

--- manifests/kernel.pp
+++ manifests/kernel.pp
@@ -5,6 +5,7 @@ class hosting_basesetup::kernel (
   Hash $sysctl_config           = {},
   Array[String] $ulimit_config  = [],
   Boolean $sysctl_enable_fastnetworking_defaults = false,
+  Boolean $sysctl_enable_tcp_timeout_optimzation = false,
   Boolean $sysctl_ignore_defaults = false,
   Boolean $ulimit_ignore_defaults = false,
   ) {
@@ -18,65 +19,45 @@ class hosting_basesetup::kernel (
     path        => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
   }
 
-  $sysctl_stdkernel = {
-    'kernel.panic'         => {
-      'value'                => 30
-    }
-    ,
-    'kernel.panic_on_oops' => {
-      'value'                => 30
-    }
-    ,
-    'vm.panic_on_oom'      => {
-      'value'                => 1
-    }
-    ,
-    'vm.swappiness'        => {
-      'value'                => 0
-    }
-    ,
-    'fs.aio-max-nr'        => {
-      'value'                => 262144
-    }
-    ,
-    'fs.file-max'          => {
-      'value'                => 1000000
-    }
-    ,
+  $sysctl_stdkernel = { 
+    'kernel.panic'=> { 'value'=> 30 } ,
+    'kernel.panic_on_oops' => { 'value'=> 30 } ,
+    'vm.panic_on_oom'=> { 'value'=> 1 } ,
+    'vm.swappiness'=> { 'value'=> 0 } ,
+    'fs.aio-max-nr'=> { 'value'=> 262144 },
+    'fs.file-max'=> { 'value'=> 1000000 },
   }
-
+  
   #################################################################################
-  # # Settings for 10G NIC, optimized for network paths up to 100ms RTT,
+  # TCP TIMEOUT Optimization
+  # During low traffic intervals, a firewall configured with an idle connection 
+  # timeout can close connections to local nodes and nodes in other data centers.
+  if $sysctl_enable_tcp_timeout_optimzation {
+    $sysctl_tcp_timout_optimization_defaults = {
+      'net.ipv4.tcp_keepalive_time' => {'value' => '60'}, 
+      'net.ipv4.tcp_keepalive_probes' => {'value' => '3'},
+      'net.ipv4.tcp_keepalive_intvl' => {'value' => '10'},
+    }
+  }else{
+    $sysctl_tcp_timout_optimization_defaults = {
+    }
+  }
+  #################################################################################
+  # Settings for 10G NIC/fast networks, optimized for network paths up to 100ms RTT,
+
   if $sysctl_enable_fastnetworking_defaults {
     notice("Enabling networking settings for 10g equipment")
     $sysctl_fastnetworking_defaults = {
-      'net.ipv4.tcp_syncookies'     => {
-        'value' => 1
-      }
-      ,
-      'net.core.rmem_max'           => {
-        'value' => 16777216
-      }
-      ,
-      'net.core.wmem_max'           => {
-        'value' => 16777216
-      }
-      ,
-      'net.ipv4.tcp_rmem'           => {
-        'value' => '4096 87380 16777216'
-      }
-      ,
-      'net.ipv4.tcp_wmem'           => {
-        'value' => '4096 65536 16777216'
-      }
-      ,
-      'net.core.netdev_max_backlog' => {
-        'value' => '30000'
-      }
-      ,
-      'net.ipv4.tcp_mtu_probing'    => {
-        'value' => '1'
-      }
+      'net.ipv4.tcp_syncookies'     => { 'value' => '1'},
+      'net.core.rmem_default        => { 'value' => '16777216'}
+      'net.core.wmem_default        => { 'value' => '16777216'}
+      'net.core.rmem_max'           => { 'value' => 16777216},
+      'net.core.wmem_max'           => { 'value' => 16777216 },
+      'net.ipv4.tcp_rmem'           => { 'value' => '4096 87380 16777216'},
+      'net.ipv4.tcp_wmem'           => { 'value' => '4096 65536 16777216'},
+      'net.core.netdev_max_backlog' => { 'value' => '30000'},
+      'net.ipv4.tcp_mtu_probing'    => { 'value' => '1'}
+      'net.core.optmem_max'         => { 'value' => '40960' }
       ,
     }
   } else {
@@ -87,7 +68,12 @@ class hosting_basesetup::kernel (
   if $sysctl_ignore_defaults {
     $sysctl_config_final = $sysctl_config
   } else {
-    $sysctl_config_final = deep_merge($sysctl_stdkernel, $sysctl_config, $sysctl_fastnetworking_defaults)
+    $sysctl_config_final = deep_merge(
+        $sysctl_stdkernel, 
+        $sysctl_config, 
+        $sysctl_fastnetworking_defaults, 
+        $sysctl_tcp_timout_optimization_defaults
+        )
   }
   create_resources('hosting_basesetup::kernel::sysctl', $sysctl_config_final)
   
