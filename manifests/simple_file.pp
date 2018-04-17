a70e8e9353513347e5a8173f22564d6387391e14
Marc Schoechlin <ms@256bit.org>
2018-04-17 21:25:07 +0200

    Simple files

---
+++ manifests/simple_file.pp
@@ -0,0 +1,19 @@
+define hosting_basesetup::simple_file(
+  String $filename     = $title,
+  Hash   $config,
+  String $template,
+  String $owner   = 'root',
+  String $group   = 'root',
+  String $mode    = '0600',
+  Boolean $backup  = false,
+  String $ensure        = present,) {
+
+  file { $filename:
+      ensure  => $ensure,
+      owner   => $owner,
+      group   => $owner,
+      mode    => $mode,
+      backup  => $backup,
+      content => template($template),
+  }
+}
