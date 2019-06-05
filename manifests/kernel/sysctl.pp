63c52b00239502eee4cac051bc04ea0d433cbfb9
Amin Dandache <amin.dandache@vico-research.com>
2019-06-05 15:29:46 +0200

    network performance improvements and low latency

--- manifests/kernel/sysctl.pp
+++ manifests/kernel/sysctl.pp
@@ -3,6 +3,8 @@ class hosting_basesetup::kernel::sysctl (
   Hash    $config                                     = $::hosting_basesetup::kernel::sysctl_config,
   Boolean $enable_fastnetworking_defaults             =
     $::hosting_basesetup::kernel::sysctl_enable_fastnetworking_defaults,
+  Boolean $enable_lowlatencynetworking_defaults       =
+    $::hosting_basesetup::kernel::sysctl_enable_lowlatencynetworking_defaults,
   Boolean $enable_tcp_timeout_optimzation             =
     $::hosting_basesetup::kernel::sysctl_enable_tcp_timeout_optimzation,
   Boolean $enable_automatic_reboot_on_kernel_problems =
@@ -72,21 +74,41 @@ class hosting_basesetup::kernel::sysctl (
   if $enable_fastnetworking_defaults {
     notice("Enabling networking settings for 10g equipment")
     $fastnetworking_defaults = {
-      'net.core.netdev_max_backlog' => { 'value' => '30000' },
-      'net.core.rmem_default'       => { 'value' => '16777216' },
-      'net.core.wmem_default'       => { 'value' => '16777216' },
-      'net.core.rmem_max'           => { 'value' => '16777216' },
-      'net.core.wmem_max'           => { 'value' => '16777216' },
-      'net.ipv4.tcp_rmem'           => { 'value' => '4096 87380 16777216' },
-      'net.ipv4.tcp_wmem'           => { 'value' => '4096 65536 16777216' },
-      'net.ipv4.tcp_syncookies'     => { 'value' => '1' },
-      'net.ipv4.tcp_mtu_probing'    => { 'value' => '1' },
-      'net.core.optmem_max'         => { 'value' => '40960' },
+      'net.core.somaxconn'           => { 'value' => '1024' },    # ori 128
+      'net.core.netdev_max_backlog'  => { 'value' => '250000' },
+      'net.core.rmem_default'        => { 'value' => '134217728' },
+      'net.core.wmem_default'        => { 'value' => '134217728' },
+      'net.core.rmem_max'            => { 'value' => '134217728' },
+      'net.core.wmem_max'            => { 'value' => '134217728' },
+      'net.core.optmem_max'          => { 'value' => '40960' },
+      'net.ipv4.tcp_rmem'            => { 'value' => '4096 87380 67108864' },
+      'net.ipv4.tcp_wmem'            => { 'value' => '4096 65536 67108864' },
+      'net.ipv4.tcp_syncookies'      => { 'value' => '1' },
+      'net.ipv4.tcp_mtu_probing'     => { 'value' => '1' },
+      'net.ipv4.tcp_max_syn_backlog' => { 'value' => '30000' },   # ori 2048
+      'net.ipv4.tcp_max_tw_buckets'  => { 'value' => '2000000' }, # ori 262144
+      'net.ipv4.tcp_tw_reuse'        => { 'value' => '1' },       # ori 1
+      'net.ipv4.tcp_fin_timeout'     => { 'value' => '10' },      # ori 60
     }
   } else {
     $fastnetworking_defaults = {
     }
   }
+
+  #################################################################################
+  # Settings for Low Latency Networking (latency vs. throughput)
+
+  if $enable_lowlatencynetworking_defaults {
+    notice("Enabling low latency networking settings")
+    $lowlatencynetworking_defaults = {
+      'net.ipv4.tcp_timestamps'     => { 'value' => '0' },
+      'net.ipv4.tcp_low_latency'    => { 'value' => '1' },
+    }
+  } else {
+    $lowlatencynetworking_defaults = {
+    }
+  }
+
 
   #################################################################################
   # Combine and create ressources
@@ -98,6 +120,7 @@ class hosting_basesetup::kernel::sysctl (
       $stdkernel,
       $config,
       $fastnetworking_defaults,
+      $lowlatencynetworking_defaults,
       $tcp_timout_optimization_defaults,
       $automatic_reboot_on_kernel_problems,
     )
