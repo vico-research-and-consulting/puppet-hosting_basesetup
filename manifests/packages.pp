751aad228b1c3d3b3a40ee588f27354865fab29d
Marc Schoechlin <marc.schoechlin@vico-research.com>
2020-06-10 09:15:40 +0200

    add multitail tool

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -8,7 +8,7 @@ class hosting_basesetup::packages (
       $packages = [
         'apt-transport-https', 'bash-completion', 'curl', 'wget', 'htop', 'iftop', 'at', 
         'iptraf-ng', 'pv', 'lsscsi', 'dialog', 'ncdu', 'scsitools', 'sysstat', 'xfsdump',
-        'xfsprogs', 'zip', 'git', 'tig', 'vim', 'finger', 'mailutils', 'traceroute',
+        'xfsprogs', 'zip', 'git', 'tig', 'vim', 'finger', 'mailutils', 'traceroute', 'multitail',
         'net-tools', 'lvm2', 'strace', 'sysfsutils', 'screen', 'nuttcp', 'gnupg', 'nethogs', "pigz",
       ]
       if (!$::is_virtual) {
